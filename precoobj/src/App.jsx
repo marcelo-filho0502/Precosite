e0b8825f07f2271087d5323b879ff11f1cebf210
marcelo-filho0502 <marcelomrfilho@gmail.com>
2025-10-21 22:31:59 -0300

    comeco

---
+++ precoobj/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MainContent from './components/MainContent'
+
+function App() {
+return (
+ 
+      
+      <main>
+        <MainContent />
+      </main>
+)  
+  
+}
+
+
+export default App
---
+++ precoobj/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+export default MainContent;
+
+function MainContent() {
+
+  
+  return (
+    <div className="main-container">
+      
+        <p><h1>{"Demolidor"}</h1></p>
+        
+    </div>
+  );
+}
+
+
